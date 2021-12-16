bd15b443e374d5e0a8502d92f612d62dc1f52263
deepakkoodali <92927878+deepakkoodali@users.noreply.github.com>
2021-12-16 13:03:28 +0530

    Day 21 Activity

---
+++ js/script.js
@@ -0,0 +1,114 @@
+
+
+const timer = document.getElementById('stopwatch');
+
+let sec = 11;
+let stoptime = true;
+let word = "Happy Independence Day"
+
+let startTimer = () => {
+  if (stoptime == true) {
+        stoptime = false;
+        timerCycle();
+    }
+}
+
+
+let timerCycle = () => {
+  if (stoptime == false) {
+
+    printTime();
+
+    setTimeout(() => {
+      if (stoptime == false) {
+
+        printTime();
+
+        setTimeout(() => {
+          if (stoptime == false) {
+
+            printTime();
+
+            setTimeout(() => {
+              if (stoptime == false) {
+
+                printTime();
+
+                setTimeout(() => {
+                  if (stoptime == false) {
+
+                    printTime();
+
+                    setTimeout(() => {
+                      if (stoptime == false) {
+
+                        printTime();
+
+                        setTimeout(() => {
+                          if (stoptime == false) {
+
+                            printTime();
+
+                            setTimeout(() => {
+                              if (stoptime == false) {
+
+                                printTime();
+
+                                setTimeout(() => {
+                                  if (stoptime == false) {
+
+                                    printTime();
+
+                                    setTimeout(() => {
+                                      if (stoptime == false) {
+
+                                        printTime();
+
+                                        setTimeout(() => {
+                                          if (stoptime == false) {
+
+                                            printTime();
+                                          }
+                                        }, 1000);
+                                      }
+                                    }, 1000);
+                                  }
+                                }, 1000);
+                              }
+                            }, 1000);
+                          }
+                        }, 1000);
+                      }
+                    }, 1000);
+                  }
+                }, 1000);
+              }
+            }, 1000);
+          }
+        }, 1000);
+      }
+    }, 1000);
+  }
+}
+
+
+let printTime = () => {
+  sec = parseInt(sec);
+
+    sec = sec - 1;
+
+    if (sec < 10) {
+      sec = '0' + sec;
+      timer.innerHTML = sec;
+    }
+    if(sec == 10){
+        timer.innerHTML = "10"
+    }
+    if(sec == 0){
+        stoptime = true;
+        timer.innerHTML = word;
+        sec = 11;
+    }
+}
+
+startTimer();
